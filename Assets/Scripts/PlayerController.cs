6dccefc9001a919868ac6bf4f0e7f721a4c25d00
Shouxialiuqing <481719161@qq.com>
2025-06-23 22:21:15 +0800

    整理内容，修了一个小bug

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using DG.Tweening;
+[SelectionBase]
+public class PlayerController : MonoBehaviour
+{
+    public bool walking = false;
+    private bool isSearching = false;
+    [Space]
+
+    public Transform currentCube;
+    public Transform clickedCube;
+    public Transform indicator;
+
+    [Space]
+
+    public List<Transform> finalPath = new List<Transform>();
+
+    private float blend;//上下楼梯极值控制
+
+    void Start()
+    {
+        RayCastDown();
+    }
+
+    void Update()
+    {
+        //GET CURRENT CUBE (UNDER PLAYER) 获取玩家当前所在的方块
+
+        RayCastDown();
+
+        if (isSearching) return;
+        //确保物体旋转时玩家跟随着一起动
+        if (currentCube.GetComponent<Walkable>().movingGround)
+        {
+            transform.parent = currentCube.parent;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        // CLICK ON CUBE
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition); RaycastHit mouseHit;
+
+            if (Physics.Raycast(mouseRay, out mouseHit))
+            {
+                if (mouseHit.transform.GetComponent<Walkable>() != null)
+                {
+                    isSearching = true;
+                    clickedCube = mouseHit.transform;
+                    DOTween.Kill(gameObject.transform);
+                    finalPath.Clear();
+                    FindPath();
+
+                    blend = transform.position.y - clickedCube.position.y > 0 ? -1 : 1;
+
+                    indicator.position = mouseHit.transform.GetComponent<Walkable>().GetWalkPoint();
+                    Sequence s = DOTween.Sequence();
+                    s.AppendCallback(() => indicator.GetComponentInChildren<ParticleSystem>().Play());
+                    s.Append(indicator.GetComponent<Renderer>().material.DOColor(Color.white, .1f));
+                    s.Append(indicator.GetComponent<Renderer>().material.DOColor(Color.black, .3f).SetDelay(.2f));
+                    s.Append(indicator.GetComponent<Renderer>().material.DOColor(Color.clear, .3f));
+
+                }
+            }
+        }
+    }
+
+    void FindPath()
+    {
+        
+        List<Transform> nextCubes = new List<Transform>();
+        List<Transform> pastCubes = new List<Transform>();
+
+        foreach (WalkPath path in currentCube.GetComponent<Walkable>().possiblePaths)
+        {
+            if (path.active)
+            {
+                nextCubes.Add(path.target);
+                path.target.GetComponent<Walkable>().previousBlock = currentCube;
+            }
+        }
+
+        pastCubes.Add(currentCube);
+
+        ExploreCube(nextCubes, pastCubes);
+        BuildPath();
+    }
+
+    void ExploreCube(List<Transform> nextCubes, List<Transform> visitedCubes)
+    {
+        Transform current = nextCubes.First();
+        nextCubes.Remove(current);
+
+        if (current == clickedCube)
+        {
+            return;
+        }
+
+        foreach (WalkPath path in current.GetComponent<Walkable>().possiblePaths)
+        {
+            if (!visitedCubes.Contains(path.target) && path.active)
+            {
+                nextCubes.Add(path.target);
+                path.target.GetComponent<Walkable>().previousBlock = current;
+            }
+        }
+
+        visitedCubes.Add(current);
+
+        if (nextCubes.Any())
+        {
+            ExploreCube(nextCubes, visitedCubes);
+        }
+    }
+
+    void BuildPath()
+    {
+        Transform cube = clickedCube;
+        while (cube != currentCube)
+        {
+            finalPath.Add(cube);
+            if (cube.GetComponent<Walkable>().previousBlock != null)
+                cube = cube.GetComponent<Walkable>().previousBlock;
+            else
+                return;
+        }
+
+        finalPath.Insert(0, clickedCube);
+        
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        Sequence s = DOTween.Sequence();
+
+        walking = true;
+
+        for (int i = finalPath.Count - 1; i > 0; i--)
+        {
+            float time = finalPath[i].GetComponent<Walkable>().isStair ? 1.5f : 1;
+
+            s.Append(transform.DOMove(finalPath[i].GetComponent<Walkable>().GetWalkPoint(), .2f * time).SetEase(Ease.Linear));
+
+            if(!finalPath[i].GetComponent<Walkable>().dontRotate)
+               s.Join(transform.DOLookAt(finalPath[i].position, .1f, AxisConstraint.Y, Vector3.up));
+        }
+
+        if (clickedCube.GetComponent<Walkable>().isButton)
+        {
+            s.AppendCallback(()=>GameManager.instance.RotateRightPivot());
+        }
+
+        s.AppendCallback(() => Clear());
+    }
+
+    void Clear()
+    {
+        isSearching = false; // 寻路结束解锁
+        foreach (Transform t in finalPath)
+        {
+            t.GetComponent<Walkable>().previousBlock = null;
+        }
+        finalPath.Clear();
+        walking = false;
+    }
+
+
+    //从玩家向下发射射线用于获取当前玩家脚下方块的信息
+    public void RayCastDown()
+    {
+
+        Ray playerRay = new Ray(transform.GetChild(0).position, -transform.up);
+        RaycastHit playerHit;
+
+        if (Physics.Raycast(playerRay, out playerHit))
+        {
+            if (playerHit.transform.GetComponent<Walkable>() != null)
+            {
+                currentCube = playerHit.transform;
+
+                if (playerHit.transform.GetComponent<Walkable>().isStair)
+                {
+                    DOVirtual.Float(GetBlend(), blend, .1f, SetBlend);//在0.1s内过渡到blend
+                }
+                else
+                {
+                    DOVirtual.Float(GetBlend(), 0, .1f, SetBlend);
+                }
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Ray ray = new Ray(transform.GetChild(0).position, -transform.up);
+        Gizmos.DrawRay(ray);
+    }
+
+    float GetBlend()
+    {
+        return GetComponentInChildren<Animator>().GetFloat("Blend");
+    }
+    void SetBlend(float x)
+    {
+        GetComponentInChildren<Animator>().SetFloat("Blend", x);
+    }
+
+}
